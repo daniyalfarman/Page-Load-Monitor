2db017c6ff444b66d1878f234b0cb34412762720
daniyalfarman <daniyal.farman@arbisoft.com>
2025-03-10 03:58:04 +0500

    Add GitHub Actions workflow for CI/CD

---
+++ cypress/e2e/1-getting-started/todo.cy.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+describe("Dummy Test Case", () => {
+  it("should visit example.com and check the title", () => {
+    // Visit the dummy website
+    cy.visit("https://example.com");
+
+    // Assert that the page title is correct
+    cy.title().should("eq", "Example Domain");
+  });
+});
